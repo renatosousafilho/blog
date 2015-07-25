bfdcb42257564c90d60b5c7e1061f08a791b8ca9
Renato Filho <renatosousafilho@gmail.com>
2015-07-25 16:57:07 -0300

    Aula do dia 25/07

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,32 @@
 class AuthorsController < ApplicationController
+	# GET /authors
 	def index
 		@authors = Author.all
+	end
+
+	# GET /authors/new
+	def new
+		@author = Author.new
+	end
+
+	# POST /authors
+	def create
+		@author = Author.new(author_params)
+		if @author.save
+			redirect_to authors_path, :notice => "Autor cadastrado com sucesso!"
+		else
+			flash[:notice] = "Autor nao pode ser cadastrado" 
+			render :new 
+		end
+	end
+
+	def edit
+		@author = Author.find(params[:id])
+	end
+
+	private
+
+	def author_params
+		params.require(:author).permit(:name, :bio)
 	end
 end
